fca92032947f2239dbb79376cf7eedba60609b9e
YXD942 <70051696+YXD942@users.noreply.github.com>
2021-03-05 18:54:47 +0800

    Add files via upload

---
+++ CrazyChess/src/CrazyChess/logic/music.java
@@ -0,0 +1,72 @@
+package CrazyChess.logic;
+
+import java.net.URL;
+import javafx.scene.media.AudioClip;
+
+/**
+ * The music class.
+ * 
+ * @author Yunlong
+ *
+ */
+
+public class music {
+	
+	// create sound for chessmove
+	public URL ulrchessmove;
+	public AudioClip chessmove;
+	
+	//create sound for Bomb
+	protected URL ulrBomb;
+	protected AudioClip Bomb;
+	
+	//create sound for Mini-Promote
+	protected URL ulrMiniPromote;
+	protected AudioClip MiniPromote;
+	
+	//create sound for Teleport
+	protected URL ulrTeleport;
+	protected AudioClip Teleport;
+	
+	
+	/**
+	 * Constructor for the music class.
+	 * Initiates every sound effect
+	 */
+	
+	public music() {
+		ulrchessmove = this.getClass().getClassLoader().getResource("resourses.music/chessmove.wav");
+		chessmove	  = new AudioClip(ulrchessmove.toExternalForm());
+		
+		ulrBomb = this.getClass().getClassLoader().getResource("resourses.music/Bomb.wav");
+		Bomb = new AudioClip(ulrBomb.toExternalForm());
+		
+        ulrMiniPromote  = this.getClass().getClassLoader().getResource("resourses.music/Mini-Promote.wav");
+		MiniPromote  = new AudioClip(ulrMiniPromote.toExternalForm());
+		
+		ulrTeleport = this.getClass().getClassLoader().getResource("resourses.music/Teleport.wav");
+		Teleport = new AudioClip(ulrTeleport.toExternalForm());
+	}
+
+	
+	//play chessmove sound
+	public void chessmove() {
+		chessmove.play();
+	}
+	
+	//play bomb sound
+	public void Bomb() {
+		Bomb.play(0.1);
+	}
+	
+	//play Mini-Promote sound
+	public void MiniPromote() {
+		MiniPromote.play();
+	}
+	
+	//play Teleport sound
+	public void Teleport() {
+		Teleport.play();
+	}
+	
+}
